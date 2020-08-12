8cb21d0a30132ac91200b4b2f98a6e5c8ccebe99
Igor <igorgronthebest@gmail.com>
2020-08-12 11:10:41 +0300

    started work with redux

--- src/comp/Posts.js
+++ src/comp/Posts.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import PostItem from './PostItem.js'
 
-export default ({posts}) => {
+const Posts = ({posts}) => {
 	if (!posts.length) {
 		return <p>Постов пока нет!</p>
 	}
 	return posts.map(post => <PostItem post={post} key={post} />
 	)
 }
+
+export default Posts
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {compose, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import {rootReducer} from './redux/rootReducer'
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer, compose(
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+const app = (
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
